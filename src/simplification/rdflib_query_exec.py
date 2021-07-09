bcc97046bc43bdd3e82353cc526fb17395f72084
robert-mieth <robert.mieth@tum.de>
2021-07-09 21:53:41 +0200

    adapt to current state of aas-spec ontology

--- src/simplification/rdflib_query_exec.py
+++ src/simplification/rdflib_query_exec.py
@@ -7,23 +7,15 @@ import sys
 def load_query(querypath):
     return open(querypath, "r").read()
 
-def simplify_aas_graph(aasfile, output, test=False, *updatequeries):
+def simplify_aas_graph(aasfile, output, *updatequeries):
     """
     :param updatequeries: ordered 2-tuples of insert and delete queries
     """
     g = rdflib.Graph()
     g.parse(aasfile, format="ttl")
-    if test:
-        test_keyfix(g)
     for uq in updatequeries:
         g.update(load_query(uq))
-    if test:
-        test_keyfix(g)
     g.serialize(destination=output, format="turtle")
-
-def test_keyfix(g):
-    print(list(g.query(load_query("complicatedSMrel.sparql"))))
-    print(list(g.query(load_query("simpleSMrel.sparql"))))
 
 def simplify_several(inputs):
     """
@@ -35,7 +27,7 @@ def simplify_several(inputs):
                   "dtfix_range_int", "dtfix_range_float", "dtfix_range_str"]
     queries = [s + ".sparql" for s in querynames]
     for i in inputs:
-        simplify_aas_graph(i, "_"+ i, True, *queries)
+        simplify_aas_graph(i, "_"+ i, *queries)
 
 if __name__ == "__main__":
     simplify_several(sys.argv[1:])
