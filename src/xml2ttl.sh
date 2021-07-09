b6f7ac6da3c844b04fc4de40a1a9afa1494bd1cb
Felix Ocker <felix.ocker@tum.de>
2021-07-09 22:25:25 +0200

    fix command chains in make file; non-verbose transformation

--- src/xml2ttl.sh
+++ src/xml2ttl.sh
@@ -9,9 +9,9 @@ for file in "$@"
 do
     path=".\/..\/data\/"
     sed -i "s/unique_input_filename_placeholder/$path$file/g" transformation/maps/XML2RDF.rml.ttl
-    java -jar transformation/rmlmapper.jar -v -m transformation/maps/XML2RDF.rml.ttl -f transformation/functions/functions.ttl transformation/functions/functions_grel.ttl transformation/functions/grel_java_mapping.ttl -s turtle -o $file.ttl
+    java -jar transformation/rmlmapper.jar -m transformation/maps/XML2RDF.rml.ttl -f transformation/functions/functions.ttl transformation/functions/functions_grel.ttl transformation/functions/grel_java_mapping.ttl -s turtle -o $file.ttl
     git checkout transformation/maps/XML2RDF.rml.ttl
-    echo >> output.ttl
+    echo >> ./../data/output.ttl
     cat $file.ttl >> ./../data/output.ttl
     rm $file.ttl
 done
